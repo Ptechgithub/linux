9bba6a070394418901e1efa08ab1a6b13355f333
Peyman <118217228+Ptechgithub@users.noreply.github.com>
2023-08-30 18:52:36 +0330

    update browser.sh

--- browser.sh
+++ browser.sh
@@ -39,6 +39,7 @@ install_chromium() {
         echo "------------------------------------------------------------------------------------------------"
         echo "Chromium installed successfully."
         IP=$(hostname -I | awk '{print $1}')
+        echo " "
         echo "Use browser with http://$IP:3000"
     fi
 }
@@ -81,6 +82,7 @@ install_firefox() {
         echo "------------------------------------------------------------------------------------------------"
         echo "Firefox installed successfully."
         IP=$(hostname -I | awk '{print $1}')
+        echo " "
         echo "Use browser with http://$IP:4000"
     fi
 }
